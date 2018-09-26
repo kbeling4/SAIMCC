b29f65151dc19df09bd364703f1f3a32da4221bf
Kyle Beling <pcxcx2@gmail.com>
2018-09-26 10:35:41 -0600

    Cleaned up code

--- SCPTP/main.py
+++ SCPTP/main.py
@@ -11,23 +11,23 @@ import Tallies as tal
 def Main():
     t0 = time.time()
     # num_threads = 2
-    nps = 1e2
+    nps = 1e6
     random.seed( 123456 )
-    
+
     # --------- problem properties ----------------
     # Physical properties
     start  =  0   # Units of cm
-    thickness = 0.5
-    
+    thickness = 0.1
+
     initialEnergy  = 1700 # Units of MeV
     energyCutOff = 1e-1
 
     particle = ['proton', 1,  938.27231]
     material = ['Tungsten', 74, 183.84, 19.3, 6e-04]
-    
+
     # Tally properties
     num_bins = 100
-    bins = np.linspace( 1680, 1700, num=num_bins)
+    bins = np.linspace( 1690, 1700, num=num_bins)
     tally = [0]*(num_bins - 1)
 
     # --------- run problem --------------------
@@ -35,7 +35,7 @@ def Main():
     initial = [ start, initialEnergy, thickness, energyCutOff ]
 
     loop.SCPT(particle, material, bins, tally, nps, initial)
-    
+
     # threads = [None]*int(num_threads)
     # for i in range(0, int(num_threads)):
     #     nps_t = nps/num_threads
@@ -54,8 +54,8 @@ def Main():
     total = t1 - t0
     print "total time = %s" % total
 
-    tal.normalizer( tally, bins, initial )
-    tal.plotter( tally, bins, 'bar' )
+    tal.normalizer( tally, bins, initial, nps )
+    tal.plotter( tally, bins, 'scatter' )
 
 
 # ---------- Run problem ---------------------
